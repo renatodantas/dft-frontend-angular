2129db48b30a9164be9df18eda689d30e01688c9
Renato Dantas <rexdantas@gmail.com>
2021-06-20 11:42:26 -0300

    First commit

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component } from '@angular/core';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+
+  isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+}
